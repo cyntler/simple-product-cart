88c9492bc4ccaa0f1ffa577fa72ad858ff06b204
Damian Cyntler <damian@cyntler.com>
2022-05-27 15:27:30 +0200

    Add Response class

---
+++ src/api/core/Response.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SimpleProductCart\Api\Core;
+
+use stdClass;
+
+class Response
+{
+    public function json($data)
+    {
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($data);
+    }
+
+    public function htmlFile($path)
+    {
+        $html = file_get_contents($path);
+        echo $html;
+    }
+
+    public function error($message)
+    {
+        http_response_code(500);
+        $stdClass = new stdClass();
+        $stdClass->errorMessage = $message;
+        echo $this->json($stdClass);
+    }
+
+    public function notFound()
+    {
+        http_response_code(404);
+        echo 'Not found!';
+    }
+}
