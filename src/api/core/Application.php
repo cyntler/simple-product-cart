0f4fd2769290ce57aa1e5aa2398b7f605dffe13c
Damian Cyntler <damian@cyntler.com>
2022-05-27 15:27:48 +0200

    Fix run method of Application class

--- src/api/core/Application.php
+++ src/api/core/Application.php
@@ -2,12 +2,27 @@
 
 namespace SimpleProductCart\Api\Core;
 
+use Exception;
+
 class Application
 {
     public static function run($urlPath, $indexHtmlFilePath)
     {
-        if (in_array($urlPath, ['', '/']) && file_exists($indexHtmlFilePath)) {
-            require_once $indexHtmlFilePath;
+        $response = new Response();
+
+        $routeClass = Utils::getApiRouteNameByUrlPath($urlPath);
+        if (!empty($routeClass)) {
+            try {
+                return $routeClass::handle($response);
+            } catch (Exception $e) {
+                return $response->error($e->getMessage());
+            }
         }
+
+        if (Utils::fileExists($indexHtmlFilePath)) {
+            return $response->htmlFile($indexHtmlFilePath);
+        }
+
+        $response->notFound();
     }
 }
