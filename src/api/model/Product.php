fd4f64cbcccaadca242c807bc8b28c9433ca0427
Damian Cyntler <damian@cyntler.com>
2022-05-27 15:28:09 +0200

    Add Product model

--- src/api/model/Product.php
+++ src/api/model/Product.php
@@ -2,30 +2,8 @@
 
 namespace SimpleProductCart\Api\Model;
 
-class Product
+use SimpleProductCart\Api\Core\Model;
+
+class Product extends Model
 {
-    private $name = '';
-    private $image;
-    private $price;
-    private $quantity;
-
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    public function setImage($image)
-    {
-        $this->image = $image;
-    }
-
-    public function setPrice($price)
-    {
-        $this->price = $price;
-    }
-
-    public function setQuantity($quantity)
-    {
-        $this->quantity = $quantity;
-    }
 }
