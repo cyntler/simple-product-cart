f2f8043546a2582cc257d1ba2b43392304b56987
Damian Cyntler <damian@cyntler.com>
2022-05-28 13:20:56 +0200

    Implement browser-sync-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,45 @@
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    path: resolve('./public/webapp'),
-    filename: 'bundle.[fullhash].js',
-    publicPath: '/webapp/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
+module.exports = (_env, { mode }) => {
+  const isDev = mode === 'development';
+
+  return {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+      path: resolve('./public/webapp'),
+      filename: 'bundle.js',
+      publicPath: '/webapp/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+        },
+        {
+          test: /\.scss$/,
+          use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        inject: 'body',
+        favicon: resolve('./src/assets/favicon.png'),
+      }),
+      new VueLoaderPlugin(),
+      isDev &&
+        new BrowserSyncPlugin({
+          host: 'localhost',
+          port: 3000,
+          proxy: 'http://localhost:3333',
+          notify: false,
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body',
-    }),
-    new VueLoaderPlugin(),
-  ],
+  };
 };
