88f4d9c2a7ae5fc4e4ee642178a2f659c625714e
Damian Cyntler <damian@cyntler.com>
2022-05-28 13:19:25 +0200

    Update App component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,17 +1,54 @@
-<script setup>
-import { ref, onMounted } from 'vue';
+<template>
+  <router-view v-slot="{ Component }">
+    <transition name="slide">
+      <component :is="Component" class="route" />
+    </transition>
+  </router-view>
+</template>
 
-const count = ref(0);
-
-function increment() {
-  count.value++;
+<style lang="scss">
+* {
+  box-sizing: border-box;
 }
 
-onMounted(() => {
-  console.log(`The initial count is ${count.value}.`);
-});
-</script>
+html,
+body,
+main {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+}
 
-<template>
-  <button @click="increment">Count is: {{ count }}</button>
-</template>
+.route {
+  width: 100%;
+  min-height: 100vh;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.75s ease-out;
+}
+
+.slide-enter-to {
+  position: absolute;
+  right: 0;
+}
+
+.slide-enter-from {
+  position: absolute;
+  right: -100%;
+}
+
+.slide-leave-to {
+  position: absolute;
+  left: -100%;
+}
+
+.slide-leave-from {
+  position: absolute;
+  left: 0;
+}
+</style>
