ef7edd12178714f909dd44fdd5e1c27c27f9a79e
Damian Cyntler <damian@cyntler.com>
2022-05-31 21:40:00 +0200

    Define ROOT_PATH

--- public/index.php
+++ public/index.php
@@ -4,15 +4,15 @@ use SimpleProductCart\Api\Core\Application;
 
 require_once '../vendor/autoload.php';
 
-$rootPath = __DIR__;
+define('ROOT_PATH', realpath(__DIR__ . '/../'));
 
 $uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], '/');
-$uriRootPath = $rootPath . '/' . $uri;
+$uriRootPath = ROOT_PATH . '/public/' . $uri;
 
 if (file_exists($uriRootPath) && is_file($uriRootPath)) {
     exit();
     return false;
 }
 
-$indexHtmlFilePath = $rootPath . '/webapp/index.html';
+$indexHtmlFilePath = ROOT_PATH . '/public/webapp/index.html';
 Application::run($uri, $indexHtmlFilePath);
