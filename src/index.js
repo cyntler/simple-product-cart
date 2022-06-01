d99e146b2aa9c53ee1016b594cf9ecd45138a90c
Damian Cyntler <damian@cyntler.com>
2022-06-01 15:10:30 +0200

    Fix google font

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './components/App.vue';
-import ProductList from './components/ProductList.vue';
-
-const vueRouter = createRouter({
-  history: createWebHistory(),
-  routes: [{ path: '/', component: ProductList }],
-});
 
 const vueApp = createApp(App);
-vueApp.use(vueRouter);
-
 vueApp.mount('#app');
